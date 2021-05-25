37e1df06cfe49abbc5ee1f0130547b3c962f7af6
aleperz <alejandraperez.659@gmail.com>
2021-05-25 00:44:26 -0500

    ajustando CLI y  test

--- src/index.js
+++ src/index.js
@@ -9,60 +9,63 @@ const path = require("path");
 const { readFile } = require("fs/promises");
 const { readdir } = require("fs/promises");
 
+const getFilesMd = (pathToGetMdFiles) =>
+  readdir(pathToGetMdFiles, { withFileTypes: true }).then((files) =>
+    Promise.all(
+      files.map((file) => {
+        const pathResolve = path.resolve(pathToGetMdFiles, file.name);
+        const fileParsed = file.isDirectory()
+          ? getFilesMd(pathResolve)
+          : pathResolve;
+        return fileParsed;
+      })
+    )
+  );
+
+const getLinks = (pathFile, pathAbsol) =>
+  readFile(pathFile).then((content) => {
+    const tokens = marked.lexer(content.toString());
+    const html = marked.parser(tokens);
+    const dom = new JSDOM(html);
+    const links = dom.window.document.querySelectorAll("a");
+    const linksHttp = [...links].filter((link) => link.href.startsWith("http"));
+    return linksHttp.map((link) => ({
+      href: link.href,
+      text: link.textContent,
+      file: pathAbsol,
+    }));
+  });
+
 const mdLinks = (filePath, options = { validate: false }) =>
   // eslint-disable-next-line consistent-return
   new Promise((resolv, rej) => {
     const existPath = fs.existsSync(filePath);
-    if (!existPath) return rej(console.error("Error:Please verify path"));
-
-    const getLinks = (pathFile) =>
-      readFile(pathFile).then((content) => {
-        const tokens = marked.lexer(content.toString());
-        const html = marked.parser(tokens);
-        const dom = new JSDOM(html);
-        const links = dom.window.document.querySelectorAll("a");
-        const linksHttp = [...links].filter((link) =>
-          link.href.startsWith("http")
-        );
-        return linksHttp.map((link) => ({
-          href: link.href,
-          text: link.textContent,
-          file: pathFile,
-        }));
-      });
-
-    const getFilesMd = (pathToGetMdFiles) =>
-      readdir(pathToGetMdFiles, { withFileTypes: true }).then((files) =>
-        Promise.all(
-          files.map((file) => {
-            const pathResolve = path.resolve(pathToGetMdFiles, file.name);
-            const fileParsed = file.isDirectory()
-              ? getFilesMd(pathResolve)
-              : pathResolve;
-            return fileParsed;
-          })
-        )
-      );
+    if (!existPath) return rej(new Error("Please verify path"));
 
     let arrayObjs;
     const extName = path.extname(filePath);
     const isDir = fs.lstatSync(filePath).isDirectory();
     if (isDir) {
       arrayObjs = getFilesMd(filePath).then((mdFiles) =>
-        /*  const flattenDeep = (arr1) =>
-          arr1.reduce(
-            (acc, val) =>
-              Array.isArray(val)
-                ? acc.concat(flattenDeep(val))
-                : acc.concat(val),
-            []
-          ); */
-        Promise.all(mdFiles.flat(300).map((Element) => getLinks(Element)))
+        Promise.all(
+          mdFiles
+            .flat(300)
+            .filter((el) => path.extname(el) === ".md")
+            .map((element) => {
+              const pathRelative = path.isAbsolute(filePath)
+                ? element
+                : path.relative(process.cwd(), element);
+              return getLinks(element, pathRelative);
+            })
+        )
       );
     } else if (extName === ".md") {
-      arrayObjs = getLinks(filePath);
+      const pathRelative = path.isAbsolute(filePath)
+        ? filePath
+        : path.relative(process.cwd(), filePath);
+      arrayObjs = getLinks(filePath, pathRelative);
     } else {
-      rej(console.error("Error: File is not .md File"));
+      rej(new Error("Error: File is not .md File"));
     }
 
     const arrayFinal = arrayObjs.then((val) => val.flat());
@@ -86,13 +89,14 @@ const mdLinks = (filePath, options = { validate: false }) =>
                   objUrl.status = error.request.status;
                   objUrl.ok = error.request.statusText;
                 } else {
-                  console.error("Error", error.message);
+                  objUrl.status = "error";
+                  objUrl.ok = error.message;
                 }
               });
           })
         )
           .then(() => resolv(arrayLink))
-          .catch((err) => rej(console.error(err)));
+          .catch((err) => rej(new Error(err)));
       });
     } else {
       arrayObjs.then((n) => resolv(n.flat()));
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,7 +1,55 @@
-const mdLinks = require("/index");
+const mdLinks = require("../src/index");
 
 describe("mdLinks", () => {
-  it("should...", () => {
-    console.log("FIX ME!");
+  it("should be a function", () => {
+    expect(typeof mdLinks).toBe("function");
+  });
+  it("should return an error if the path does not exist", () => {
+    expect.assertions(1);
+    return mdLinks("path/notExist").catch((result) => {
+      expect(result.message).toBe("Please verify path");
+    });
+  });
+  it("should return an array", () => {
+    expect.assertions(1);
+    return mdLinks("readme.md").then((result) => {
+      expect(Array.isArray(result)).toBeTruthy();
+    });
+  });
+  it("should return objectResult", () => {
+    const objResult = {
+      href: "https://nodejs.org/",
+      text: "Node.js",
+      file: "readme_test.md",
+    };
+    expect.assertions(1);
+    return mdLinks("readme_test.md").then((result) => {
+      expect(result[1]).toEqual(objResult);
+    });
+  });
+  it.skip("should return a objectValidate", () => {
+    const objValidate = {
+      href: "https://nodejs.org/es/",
+      text: "Node.js",
+      file: "readme_test.md",
+      status: 200,
+      ok: "OK",
+    };
+    expect.assertions(1);
+    return mdLinks("readme_test.md", { validate: true }).then((result) => {
+      expect(result[2]).toEqual(objValidate);
+    });
+  });
+  it("should return a promise type object", () => {
+    expect.assertions(1);
+    return mdLinks("./prueba").then((result) => {
+      expect(typeof result).toBe("object");
+    });
+  });
+  it("should return an error if the file is not .md ext", () => {
+    expect.assertions(1);
+    return mdLinks("./bin/global.js").catch((result) => {
+      expect(result.message).toBe("Error: File is not .md File");
+    });
   });
 });
